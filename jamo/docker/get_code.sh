b2b3744d9697a5b879faf2724bedc6345b41ce33
Andrew Tritt <ajtritt@lbl.gov>
2024-09-20 18:10:42 -0700

    Add stuff for buiding Docker container

---
+++ jamo/docker/get_code.sh
@@ -0,0 +1,19 @@
+git clone https://$USER:$PAT@code.jgi.doe.gov/advanced-analysis/jgi-sdm-common.git sdm-common.git
+cd sdm-common.git
+git checkout 1454897ae326c57ece6461831cd393e44a9af979 
+cd ..
+
+git clone https://$USER:$PAT@code.jgi.doe.gov/advanced-analysis/jgi-lapinpy.git lapinpy.git
+cd lapinpy.git
+git checkout a6805b40f1de33644e99dead94368cf99d630535 
+cd ..
+
+git clone https://$USER:$PAT@code.jgi.doe.gov/advanced-analysis/jgi-jamo.git jamo.git
+cd jamo.git
+git checkout 89ef9a952e253dfa35ff1ce1abb311f37838e990 
+cd ..
+
+git clone https://$USER:$PAT@code.jgi.doe.gov/advanced-analysis/jgi-jat-ui.git jat.git
+cd jat.git
+git checkout 46ab7b62eb792982616e62d2b4c54bcffe842870 
+cd ..
