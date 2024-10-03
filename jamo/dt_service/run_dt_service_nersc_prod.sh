566d7a4029b7fdfe18e3c131a3683e667aa9800f
ajtritt <ajtritt@lbl.gov>
2024-10-02 17:33:20 -0700

    Use different environment variable

--- jamo/dt_service/run_dt_service_nersc_prod.sh
+++ jamo/dt_service/run_dt_service_nersc_prod.sh
@@ -4,10 +4,10 @@ ENV=${1?"Please provide the environment to load before running"}
 
 conda activate $ENV
 
-DOMAIN=`echo $JAMO_URL | sed -e 's/https:\/\///g'`
+DOMAIN=`echo $JAMO_HOST | sed -e 's/https:\/\///g'`
 
 cd $HOME
-jamo_status=$(curl -k -s $JAMO_URL/api/tape/diskusage | grep files)
+jamo_status=$(curl -k -s $JAMO_HOST/api/tape/diskusage | grep files)
 search="^[^tmux].*dt-service.*$DOMAIN.*prod"
 
 if [[ "$jamo_status" == "" ]] ; then
@@ -31,11 +31,11 @@ elif [[ -z "$(pgrep -u $(whoami) -f $search)" ]]; then
     # use tmux attach -t <service name> to get to session
     if [ "$machine" == "dtn03.nersc.gov" ] ; then
         echo $(date) $0 start dt_service prod ingest,delete,purge,put,copy,md5,tar
-        dt-service -D t5 -t 5 -f nersc,repo_w,hsi_1,compute,globus_1 -k ingest,delete,purge,put,copy,md5,tar,untar -r 43200 $JAMO_URL -l prod
+        dt-service -D t5 -t 5 -f nersc,repo_w,hsi_1,compute,globus_1 -k ingest,delete,purge,put,copy,md5,tar,untar -r 43200 $JAMO_HOST -l prod
 
     elif [ "$machine" == "dtn04.nersc.gov" ] ; then
         echo $(date) $0 start dt_service prod starting prep,pull
-        dt-service -D t5 -t 10 -f repo_w,hsi_1,compute -k prep,pull -r 43200 $JAMO_URL -l prod
+        dt-service -D t5 -t 10 -f repo_w,hsi_1,compute -k prep,pull -r 43200 $JAMO_HOST -l prod
     fi
 
 fi
