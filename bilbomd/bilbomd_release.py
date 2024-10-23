0ab1ed8602e9c6e1eb4bad0c0f4f357bd1a2dd12
ajtritt <ajtritt@lbl.gov>
2024-10-23 11:36:58 -0700

    fix errors in bilbomd release code

--- bilbomd/bilbomd_release.py
+++ bilbomd/bilbomd_release.py
@@ -1,29 +1,35 @@
 import argparse
+import json
 import os
 import sys
 
 from jira import JIRA
+from sdm_curl import Curl
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("bilbomd_dir", help="the output directory from a BilboMD run")
 parser.add_argument("jira_issue", help="the Jira issue for this BilboMD run")
+parser.add_argument("virus_id", type=str, help="the virus the protein being analyzed belongs to")
+parser.add_argument("protein_id", type=str, help="the protein being analyzed")
 
 args = parser.parse_args()
 
 metadata_json = os.path.join(args.bilbomd_dir, 'metadata.json')
 jat_key = None
 jat_key_file = os.path.join(args.bilbomd_dir, 'jat_key')
+host = os.environ['JAMO_HOST']
 if not os.path.exists(metadata_json):
     # Build payload for posting analysis to JAMO
     outputs = []
     inputs = []
-    metadata = {'jira_issue': args.jira_issue}
+    metadata = {k: getattr(args, k) for k in ('jira_issue', 'virus_id', 'protein_id')}
+    # TODO: Pull in protein_id and virus_id
     for file in os.listdir(args.bilbomd_dir):
         path = os.path.realpath(os.path.join(args.bilbomd_dir, file))
-        file_metadata = dict{}
-        if file.startswith("ensemble_size"):
-            file_metadata['ensemble_size'] = int(file.split("_")[2])
+        file_metadata = dict()
+        if file.startswith("ensemble"):
+            file_metadata['ensemble_size'] = int(os.path.splitext(file)[0].split("_")[2])
             if file.endswith("pdb"):
                 file_metadata['file_format'] = 'pdb'
                 file_metadata['compression'] = 'none'
@@ -35,45 +41,50 @@ if not os.path.exists(metadata_json):
         elif file == 'const.inp':
             file_metadata['file_format'] = 'txt'
             file_metadata['compression'] = 'none'
+            label = 'const_inp'
         elif file.startswith('README'):
-            file_metadata['file_format'] = file.split('.')[:-1]
+            file_metadata['file_format'] = file.split('.')[-1]
             file_metadata['compression'] = 'none'
+            label = 'readme'
         elif file.startswith('multi_state_model'):
             file_metadata['ensemble_size'] = int(file.split("_")[3])
             file_metadata['file_format'] = 'saxs_dat'
             file_metadata['compression'] = 'none'
+            label = 'theoretical_saxs_curve'
         else:
             continue
         outputs.append({'file': path, 'label': label, 'metadata': file_metadata})
 
     # template data
-    td = {'outputs': outputs, 'metadata': metadata, 'inputs': inputs
+    td = {'outputs': outputs, 'metadata': metadata, 'inputs': inputs}
 
-    host = os.environ['JAMO_HOST']
     curl = Curl(host, appToken=os.environ['BILBOMD_JAMO_TOKEN'])  # BilboMD application token
     resp = curl.post('api/analysis/analysisimport',
-                     template_name='bilbomd',
-                     template_data=td},
-                     location=args.bilbomd_dir)
+                     template_name='bilbomd_classic_results',
+                     template_data=td,
+                     source="nersc",
+                     location=os.path.abspath(args.bilbomd_dir))
+
+    jat_key = resp['jat_key']
 
     if len(resp['warnings']) > 0:
         sys.stdout.write('\n'.join(resp['warnings']) + '\n')
-    print(f"Successfully imported results from {args.bilbomd_dir} as {resp['jat_key']}")
+    print(f"Successfully imported results from {args.bilbomd_dir} as {jat_key}")
 
     with open(metadata_json, 'w') as f:
         json.dump(td, f, indent=4)
 
     with open(jat_key_file, "w") as f:
-        print(resp['jat_key'], file=f)
+        print(jat_key, file=f)
 else:
     if not os.path.exists(jat_key_file):
-        print("Found metadata.json file in {args.bilbo_dir} but no JAT key file (jat_key)", file=sys.stderr)
+        print(f"Found metadata.json file in {args.bilbo_dir} but no JAT key file (jat_key)", file=sys.stderr)
         exit(1)
     with open(jat_key_file, "r") as f:
         jat_key = f.read().strip()
     print("Data already submitted to JAT")
 
-jamo_url = os.path.join(host, 'analysis', resp['jat_key'])
+jamo_url = os.path.join(host, 'analysis', jat_key)
 print(f"You can view analysis at {jamo_url}")
 
 # Connect to Jira
@@ -82,6 +93,7 @@ jira_user = os.environ.get('JIRA_USER', 'ajtritt@lbl.gov')
 jira_token = os.environ['JIRA_TOKEN']  # You can create a token in Jira account settings
 
 jira = JIRA(server=jira_server, basic_auth=(jira_user, jira_token))
+issue = jira.issue(args.jira_issue)
 
 # Update Ussue field designated for connecting to the results
 # To figure out the number of the custom field of interest, go to
@@ -91,8 +103,8 @@ jira = JIRA(server=jira_server, basic_auth=(jira_user, jira_token))
 # "id=<NUMBER>". The number here will be the number to append to "customfield_"
 # for updating the field through the API.
 
-jira.issue(args.jira_issue).update(fields={'customfield_10112': jamo_url})
+issue.update(fields={'customfield_10112': jamo_url})
 print(f"Issue {args.jira_issue} updated successfully.")
 
 jira.transition_issue(issue, '41')
-print(f"Issue {jira_issue} marked as Done")
+print(f"Issue {args.jira_issue} marked as Done")
