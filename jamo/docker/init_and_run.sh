f58b1a426b1d6e288544968f7fc4d63b1693075e
Andrew Tritt <ajtritt@lbl.gov>
2024-10-25 15:46:56 -0700

    Update inputs for building Jamo service image

--- jamo/docker/init_and_run.sh
+++ jamo/docker/init_and_run.sh
@@ -2,7 +2,7 @@
 CONF=$1
 
 lapind init $CONF/lapinpy.config
-jamo-init --clean $CONF/lapinpy.config $CONF/tape.config $CONF/metadata.config
+jamo-init $CONF/lapinpy.config $CONF/tape.config $CONF/metadata.config
 jat-init $CONF/lapinpy.config $CONF/analysis.config
 
 lapind run $CONF jamo.tape jamo.metadata jat.analysis
